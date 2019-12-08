b61d23e066d838cb78419e890b8cf71b5b3abe69
james darin <jdchandler88@gmail.com>
2019-12-07 23:14:27 -0500

    tests now pass. the code makes the requirement. now, we just need to read from input

--- 1/1/fuelCalculator.h
+++ 1/1/fuelCalculator.h
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <math.h>
 
+/**
+ * Fuel required to launch a given module is based 
+ * on its mass. Specifically, to find the fuel required 
+ * for a module, take its mass, divide by three, round 
+ * down, and subtract 2.
+ **/ 
 int calculateFuel(int mass) {
-    return -1; //stub implementation
+    return (int)floor(mass/3) - 2;
 }
 
 #ifndef TEST
