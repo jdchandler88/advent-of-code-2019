df526131344102f1757314452ab726084212c890
james darin <jdchandler88@gmail.com>
2019-12-08 22:09:22 -0500

    moved main to a main file. the main function was clashing when trying to reuse modules. whelp, learned something LOLZ

--- 2/1/intcode.c
+++ 2/1/intcode.c
@@ -61,31 +61,3 @@ void executeInstruction(int* program, int* programCounter) {
             //could also exit. 
     }
 }
-
-void programStringCallback(const char* programString) {
-    //parse the program
-    int programSize;
-    const char** parsedProgram = parseString(programString, ",", &programSize);
-    //convert to integers and delete character storage
-    int* program = malloc(programSize*sizeof(int));
-    for (int i=0; i<programSize; i++) {
-        program[i] = atoi(parsedProgram[i]);
-        free((void*)parsedProgram[i]);
-    }
-    free((void*)parsedProgram);
-    //do the program
-    executeProgram(program, programSize);
-    //free the program storage
-    free((void*)program);
-}
-
-#ifndef TEST
-int main(int argc, char** argv) {
-    if (argc <= 1) {
-        exit(0);
-    }
-    FILE* file = fopen(argv[1], "r");
-    readLines(file, programStringCallback);
-    fclose(file);
-}
-#endif
---
+++ 2/1/main.c
@@ -0,0 +1,30 @@
+#include "intcode.h"
+#include "utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+static void programStringCallback(const char* programString) {
+    //parse the program
+    int programSize;
+    const char** parsedProgram = parseString(programString, ",", &programSize);
+    //convert to integers and delete character storage
+    int* program = malloc(programSize*sizeof(int));
+    for (int i=0; i<programSize; i++) {
+        program[i] = atoi(parsedProgram[i]);
+        free((void*)parsedProgram[i]);
+    }
+    free((void*)parsedProgram);
+    //do the program
+    executeProgram(program, programSize);
+    //free the program storage
+    free((void*)program);
+}
+
+int main(int argc, char** argv) {
+    if (argc <= 1) {
+        exit(0);
+    }
+    FILE* file = fopen(argv[1], "r");
+    readLines(file, programStringCallback);
+    fclose(file);
+}
