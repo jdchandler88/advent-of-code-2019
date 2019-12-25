d76c96fe7fae468c8cab9221aa462bc3be1d9268
jdchandler88 <jdchandler88@gmail.com>
2019-12-24 21:35:10 -0500

    got dat answer

--- 10/1/main.c
+++ 10/1/main.c
@@ -6,10 +6,14 @@
 
 
 int main(int argc, char** argv) {
-    if (argc != 3) {
+    if (argc != 2) {
         printf("gimme an input file, number of amplifiers we're decoding, feedbackMode, inputOffset. For example './exe input.txt 5 1 0'\n");
         printf("\tinputFile should be a string pointing to a file\n");
-        printf("\tinput. this is used to pass to the BOOST program\n");
         exit(0);
     }
+    FILE* f = fopen(argv[1], "r");
+    struct Map map = parseMap(f);
+    int sightings = 0;
+    struct Coordinate location = maxAsteroidsVisibleLocation(&sightings, map);
+    printf("max visible asteroids are %i from location (%i,%i)\n", sightings, location.x, location.y);
 }
