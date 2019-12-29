090e43a959842849c1cb00227d0f4f5de486e1e0
jdchandler88 <jdchandler88@gmail.com>
2019-12-28 21:30:59 -0500

    completed code for answer. got mah star. w00000t

--- 10/2/main.c
+++ 10/2/main.c
@@ -4,6 +4,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+int count = 0;
+
+/**
+ * The Elves are placing bets on which will be the 200th asteroid to be vaporized. 
+ * Win the bet by determining which asteroid that will be; what do you get if you 
+ * multiply its X coordinate by 100 and then add its Y coordinate? (For example, 8,2 becomes 802.)
+ **/ 
+void destructionCallback(struct Coordinate location) {
+    count++;
+    if (count == 200) {
+        printf("200th asteroid located at (%i,%i). Answer=%i\n", location.x, location.y, ((location.x*100) + location.y));
+    }
+}
 
 int main(int argc, char** argv) {
     if (argc != 2) {
@@ -16,4 +29,6 @@ int main(int argc, char** argv) {
     int sightings = 0;
     struct Coordinate location = maxAsteroidsVisibleLocation(&sightings, map);
     printf("max visible asteroids are %i from location (%i,%i)\n", sightings, location.x, location.y);
+    printf("destroying all asteroids at location.\n");
+    destroyAsteroids(map, location, destructionCallback);
 }
